0daa02590bb93f75598734fa98d664a4d8e0bb81
Liubov Dav <lubasha0512@gmail.com>
2024-10-08 20:14:30 -0700

    Implemented wave_05.

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -2,14 +2,30 @@ import uuid
 
 class Item:
     
-    def __init__(self, id=None):
+    def __init__(self, id=None, condition=0):
         if id == None:
             self.id = uuid.uuid4().int
         else:
             self.id = id
+        self.condition = condition
 
     def get_category(self):
         return "Item"
     
     def __str__(self):
         return f"An object of type Item with id {self.id}."
+
+    def condition_description(self):
+        if self.condition == 5:
+            return "Best"
+        if self.condition == 4:
+            return "Very good"
+        if self.condition == 3:
+            return "Questionable quality, but you can try"
+        if self.condition == 2:
+            return "Only if you need this particular product"
+        if self.condition == 1:
+            return "I strongly do not recommend it"
+        if self.condition == 0:
+            return "Bad"
+ 
